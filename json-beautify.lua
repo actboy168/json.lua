eaa53f525fd144955278e1dc0e74c771210e10c0
actboy168 <actboy168@gmail.com>
2021-10-15 21:44:01 +0800

    同步代码

--- json-beautify.lua
+++ json-beautify.lua
@@ -7,10 +7,9 @@ local table_sort = table.sort
 local string_rep = string.rep
 local math_type = math.type
 local setmetatable = setmetatable
-local getmetatable = getmetatable
 
-local statusMark
-local statusQue
+local statusVisited
+local statusBuilder
 local statusDep
 local statusOpt
 
@@ -21,7 +20,7 @@ local defaultOpt = {
 defaultOpt.__index = defaultOpt
 
 local function encode_newline()
-    statusQue[#statusQue+1] = statusOpt.newline..string_rep(statusOpt.indent, statusDep)
+    statusBuilder[#statusBuilder+1] = statusOpt.newline..string_rep(statusOpt.indent, statusDep)
 end
 
 local encode_map = {}
@@ -32,12 +31,12 @@ end
 
 local function encode(v)
     local res = encode_map[type(v)](v)
-    statusQue[#statusQue+1] = res
+    statusBuilder[#statusBuilder+1] = res
 end
 
 function encode_map.string(v)
-    statusQue[#statusQue+1] = '"'
-    statusQue[#statusQue+1] = encode_string(v)
+    statusBuilder[#statusBuilder+1] = '"'
+    statusBuilder[#statusBuilder+1] = encode_string(v)
     return '"'
 end
 
@@ -50,10 +49,10 @@ function encode_map.table(t)
             return "[]"
         end
     end
-    if statusMark[t] then
+    if statusVisited[t] then
         error("circular reference")
     end
-    statusMark[t] = true
+    statusVisited[t] = true
     if type(first_val) == 'string' then
         local key = {}
         for k in next, t do
@@ -63,65 +62,82 @@ function encode_map.table(t)
             key[#key+1] = k
         end
         table_sort(key)
-        statusQue[#statusQue+1] = "{"
+        statusBuilder[#statusBuilder+1] = "{"
         statusDep = statusDep + 1
         encode_newline()
         local k = key[1]
-        statusQue[#statusQue+1] = '"'
-        statusQue[#statusQue+1] = encode_string(k)
-        statusQue[#statusQue+1] = '": '
+        statusBuilder[#statusBuilder+1] = '"'
+        statusBuilder[#statusBuilder+1] = encode_string(k)
+        statusBuilder[#statusBuilder+1] = '": '
         encode(t[k])
         for i = 2, #key do
             local k = key[i]
-            statusQue[#statusQue+1] = ","
+            statusBuilder[#statusBuilder+1] = ","
             encode_newline()
-            statusQue[#statusQue+1] = '"'
-            statusQue[#statusQue+1] = encode_string(k)
-            statusQue[#statusQue+1] = '": '
+            statusBuilder[#statusBuilder+1] = '"'
+            statusBuilder[#statusBuilder+1] = encode_string(k)
+            statusBuilder[#statusBuilder+1] = '": '
             encode(t[k])
         end
         statusDep = statusDep - 1
         encode_newline()
-        statusMark[t] = nil
+        statusVisited[t] = nil
         return "}"
-    else
-        local count = 0
+    elseif json.supportSparseArray then
         local max = 0
         for k in next, t do
             if math_type(k) ~= "integer" or k <= 0 then
                 error("invalid table: mixed or invalid key types")
             end
-            count = count + 1
             if max < k then
                 max = k
             end
         end
-        if not json.supportSparseArray and count ~= max then
-            error("sparse array are not supported")
-        end
-        statusQue[#statusQue+1] = "["
+        statusBuilder[#statusBuilder+1] = "["
         statusDep = statusDep + 1
         encode_newline()
         encode(t[1])
         for i = 2, max do
-            statusQue[#statusQue+1] = ","
+            statusBuilder[#statusBuilder+1] = ","
             encode_newline()
             encode(t[i])
         end
         statusDep = statusDep - 1
         encode_newline()
-        statusMark[t] = nil
+        statusVisited[t] = nil
+        return "]"
+    else
+        if t[1] == nil then
+            error("invalid table: mixed or invalid key types")
+        end
+        statusBuilder[#statusBuilder+1] = "["
+        statusDep = statusDep + 1
+        encode_newline()
+        encode(t[1])
+        local count = 2
+        while t[count] ~= nil do
+            statusBuilder[#statusBuilder+1] = ","
+            encode_newline()
+            encode(t[count])
+            count = count + 1
+        end
+        if next(t, count-1) ~= nil then
+            error("invalid table: mixed or invalid key types")
+        end
+        statusDep = statusDep - 1
+        encode_newline()
+        statusVisited[t] = nil
         return "]"
     end
 end
 
 local function beautify(v, option)
-    statusMark = {}
-    statusQue = {}
+    statusVisited = {}
+    statusBuilder = {}
     statusDep = 0
     statusOpt = option and setmetatable(option, defaultOpt) or defaultOpt
     encode(v)
-    return table_concat(statusQue)
+    return table_concat(statusBuilder)
 end
 
 json.beautify = beautify
